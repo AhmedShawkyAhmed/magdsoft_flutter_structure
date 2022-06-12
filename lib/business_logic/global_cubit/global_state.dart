b0f6b1f90645af20537efbd9f4a7d260901079a4
AhmedMuhammed <92108624+karar0120@users.noreply.github.com>
2022-06-12 17:36:04 +0200

    Add files via upload

--- lib/business_logic/global_cubit/global_state.dart
+++ lib/business_logic/global_cubit/global_state.dart
@@ -4,3 +4,24 @@ part of 'global_cubit.dart';
 abstract class GlobalState {}
 
 class GlobalInitial extends GlobalState {}
+
+class RegisterLoaded extends GlobalState {}
+
+class RegisterEnter extends GlobalState {}
+
+class RegisterError extends GlobalState {}
+
+class LoginLoaded extends GlobalState {}
+
+class LoginEnter extends GlobalState {
+  final LoginModal loginModal;
+  LoginEnter(this.loginModal);
+}
+
+class LoginError extends GlobalState {}
+
+class PasswordObscureText extends GlobalState{}
+
+class PasswordObscureconfiromRegisterText extends GlobalState{}
+
+class PasswordObscureLoginText extends GlobalState{}
