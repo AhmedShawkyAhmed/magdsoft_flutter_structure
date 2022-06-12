79b365e8ca7a0ab2c7fa22c3151d0799462cd752
AhmedMuhammed <92108624+karar0120@users.noreply.github.com>
2022-06-12 17:34:31 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,136 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_translate/flutter_translate.dart';
+import 'package:magdsoft_flutter_structure/business_logic/bloc_observer.dart';
+import 'package:magdsoft_flutter_structure/business_logic/global_cubit/global_cubit.dart';
+import 'package:magdsoft_flutter_structure/data/local/cache_helper.dart';
+import 'package:magdsoft_flutter_structure/data/remote/dio_helper.dart';
+import 'package:magdsoft_flutter_structure/presentation/router/app_router.dart';
+import 'package:magdsoft_flutter_structure/presentation/styles/colors.dart';
+import 'package:magdsoft_flutter_structure/presentation/widget/toast.dart';
+import 'package:sizer/sizer.dart';
+import 'package:intl/intl.dart';
+
+
+late LocalizationDelegate delegate;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  BlocOverrides.runZoned(
+        () async {
+          SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+            statusBarColor: Colors.transparent,
+            systemNavigationBarColor: Colors.black,
+            statusBarBrightness: Brightness.dark,
+            statusBarIconBrightness: Brightness.dark,
+            systemNavigationBarIconBrightness: Brightness.light,
+
+          ));
+          SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+      DioHelper.init();
+      await CacheHelper.init();
+      await ScreenUtil.ensureScreenSize();
+      final locale =
+          CacheHelper.getDataFromSharedPreference(key: 'language') ?? "ar";
+      delegate = await LocalizationDelegate.create(
+        fallbackLocale: locale,
+        supportedLocales: ['ar', 'en'],
+      );
+      await delegate.changeLocale(Locale(locale));
+
+
+      runApp(  MyApp(
+        appRouter: AppRouter(),
+      ));
+    },
+    blocObserver: MyBlocObserver(),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  final AppRouter appRouter;
+
+  const MyApp({
+    required this.appRouter,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    Intl.defaultLocale = delegate.currentLocale.languageCode;
+
+
+    delegate.onLocaleChanged = (Locale value) async {
+      try {
+        setState(() {
+          Intl.defaultLocale = value.languageCode;
+        });
+      } catch (e) {
+        showToast(e.toString());
+      }
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: ((context) => GlobalCubit()),
+        ),
+      ],
+      child: BlocConsumer<GlobalCubit, GlobalState>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          return Sizer(
+            builder: (context, orientation, deviceType) {
+              return LocalizedApp(
+                delegate,
+                 MaterialApp(
+                   color: AppColor.white,
+                    debugShowCheckedModeBanner: false,
+                    title: 'Werash',
+                    localizationsDelegates: [
+                      GlobalCupertinoLocalizations.delegate,
+                      DefaultCupertinoLocalizations.delegate,
+                      GlobalMaterialLocalizations.delegate,
+                      GlobalWidgetsLocalizations.delegate,
+                      delegate,
+                    ],
+                    //locale: delegate.currentLocale,
+                    supportedLocales: delegate.supportedLocales,
+                    onGenerateRoute: widget.appRouter.onGenerateRoute,
+
+                    theme: ThemeData(
+                      fontFamily: 'cairo',
+                      //scaffoldBackgroundColor: AppColors.white,
+                      appBarTheme: const AppBarTheme(
+                        elevation: 0.0,
+                        systemOverlayStyle: SystemUiOverlayStyle(
+                          //statusBarColor: AppColors.transparent,
+                          statusBarIconBrightness: Brightness.dark,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
